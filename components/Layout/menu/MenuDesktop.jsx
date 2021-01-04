b44c8ba11224fec64fd56453244efc281327e7c6
Paulo <paulodigitalart@gmail.com>
2021-01-04 17:18:07 -0300

    update

--- components/Layout/menu/MenuDesktop.jsx
+++ components/Layout/menu/MenuDesktop.jsx
@@ -2,7 +2,7 @@ export default function MenuDesktop(){
     return(
         <div 
             className="
-           
+           text-8xlt font-bold font-mono
             
             ">Menu Desktop</div>
     )
